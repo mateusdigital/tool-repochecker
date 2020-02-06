495b54826934363db8602059bfdc7f80467fb14c
stdmatt <stdmatt@pixelwizards.io>
2020-02-06 13:17:51 +0300

    Add the setup.py script

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import setuptools
+
+setuptools.setup(
+    name="repochecker",
+    version="0.0.1",
+    description="Git repository checkekr",
+    keywords="git",
+    author="stdmatt",
+    author_email="stdmatt@pixelwizards.io",
+    url="http://stdmatt.com",
+    license="GPLv3",
+    packages=setuptools.find_packages(),
+    install_requires=["pathlib"],
+    setup_requires=[""],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+    ],
+    entry_points={
+        "console_scripts": [
+            "repochecker = repochecker.main:main"
+        ],
+    },
+)
