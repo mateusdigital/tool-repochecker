21778be976faf2c6896433d3416ae98e00fbcc26
stdmatt <stdmatt@pixelwizards.io>
2020-04-09 15:16:11 +0300

    Add the license headers

--- repochecker/colors/colors.py
+++ repochecker/colors/colors.py
@@ -1,3 +1,20 @@
+##~---------------------------------------------------------------------------##
+##                        _      _                 _   _                      ##
+##                    ___| |_ __| |_ __ ___   __ _| |_| |_                    ##
+##                   / __| __/ _` | '_ ` _ \ / _` | __| __|                   ##
+##                   \__ \ || (_| | | | | | | (_| | |_| |_                    ##
+##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
+##                                                                            ##
+##  File      : colors.py                                                     ##
+##  Project   : repochecker                                                   ##
+##  Date      : Apr 09, 2020                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
+##  Copyright : stdmatt - 2020                                                ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
 from mcow_py_termcolor import termcolor;
 
 termcolor.color_mode = termcolor.COLOR_MODE_ALWAYS;
