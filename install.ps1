9356e231108dda81fa47ed6a6194196b451206a6
stdmatt <stdmatt@pixelwizards.io>
2021-05-01 20:05:27 +0300

    [REFACTOR] Improve the install script as the other tools.

--- install.ps1
+++ install.ps1
@@ -10,7 +10,9 @@ $SCRIPT_DIR      = Split-Path "$SCRIPT_FULLPATH" -Parent;
 $HOME_DIR        = "$env:USERPROFILE";
 ## Program
 $PROGRAM_NAME         = "repochecker";
-$PROGRAM_INSTALL_PATH = "$HOME_DIR/.stcmatt_bin/$PROGRAM_NAME";
+$PROGRAM_SOURCE_PATH  = "$SCRIPT_DIR/$PROGRAM_NAME";
+$PROGRAM_INSTALL_PATH = "$HOME_DIR/.stdmatt_bin";
+$PROGRAM_INSTALL_SUBPATH = "$PROGRAM_INSTALL_PATH/$PROGRAM_NAME";
 
 ##
 ## Script
@@ -19,40 +21,22 @@ $PROGRAM_INSTALL_PATH = "$HOME_DIR/.stcmatt_bin/$PROGRAM_NAME";
 echo "Installing ...";
 
 ## Create the install directory...
-if (-not (Test-Path -LiteralPath $PROGRAM_INSTALL_PATH)) {
+if (-not (Test-Path -LiteralPath $PROGRAM_INSTALL_SUBPATH)) {
     echo "Creating directory at: ";
-    echo "    $PROGRAM_INSTALL_PATH";
-    New-Item -Path $PROGRAM_INSTALL_PATH -ItemType Directory;
+    echo "    $PROGRAM_INSTALL_SUBPATH";
+    $null = New-Item -Path $PROGRAM_INSTALL_SUBPATH -ItemType Directory;
 }
 
 ## Copy the file to the install dir...
-cp $SCRIPT_DIR/$PROGRAM_NAME/main.py    $PROGRAM_INSTALL_PATH/main.py
-echo "@echo off `npython3 $PROGRAM_INSTALL_PATH/main.py %1 %2 %3 %4 %5 %6 %7 %8 %9" | Out-File -Encoding ASCII -FilePath $PROGRAM_INSTALL_PATH/repochecker.bat
+cp -Force $PROGRAM_SOURCE_PATH/main.py     `
+          $PROGRAM_INSTALL_SUBPATH/main.py
 
+echo "@echo off `npython3 $PROGRAM_INSTALL_SUBPATH/main.py %1 %2 %3 %4 %5 %6 %7 %8 %9" `
+    | Out-File -Encoding ASCII -FilePath $PROGRAM_INSTALL_PATH/$PROGRAM_NAME.bat
 
-## @TODO(stdmatt): Implement a way to add the directory to the path so we can use
-## the repochecker from anyware... 3/16/2021, 12:43:07 PM
+echo "$PROGRAM_NAME was installed at:";
+echo "    $PROGRAM_INSTALL_PATH";
+echo "You might need add it to the PATH";
 
-
-# ##
-# ## Add the things to PATH
-# ## thanks to: https://codingbee.net/powershell/powershell-make-a-permanent-change-to-the-path-environment-variable
-# $old_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path;
-# if( $old_path.IndexOf("REPOCHECKER_DIR") -eq -1 ) {
-#     echo "Setting path...";
-
-#     $PATH_DIR = "$PROGRAM_INSTALL_PATH";
-#     $new_path = "$old_path;$PATH_DIR";
-
-#     echo "   New path: $new_path";
-
-#     [System.Environment]::SetEnvironmentVariable("REPOCHECKER_DIR", $PATH_DIR, [System.EnvironmentVariableTarget]::User);
-#     Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path;
-
-#     echo "";
-# }
-
-# echo "Done... ;D";
-# echo "";
-# Write-Host -NoNewline -Object "Press any key continue...";
-# ##$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
+echo "Done... ;D";
+echo "";
