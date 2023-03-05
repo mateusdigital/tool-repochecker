21d88de683bbd8a5e19fa86c401afe9741237ec5
mateus-earth <mateus@cosmicpig.digital>
2023-03-05 15:33:12 -0300

    Create new install script for bash and ditch the ps1

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ##----------------------------------------------------------------------------##
 ##                        _      _                 _   _                      ##
 ##                       | |    | |               | | | |                     ##
@@ -7,12 +8,12 @@
 ##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
 ##                                                                            ##
 ##                                                                            ##
-##  File      : install.ps1                                                   ##
+##  File      : install.sh                                                    ##
 ##  Project   : repochecker                                                   ##
 ##  Date      : 16 Mar, 2021                                                  ##
 ##  License   : GPLv3                                                         ##
 ##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
-##  Copyright : stdmatt - 2021, 2022                                          ##
+##  Copyright : stdmatt - 2021, 2022, 2023                                    ##
 ##                                                                            ##
 ##  Description :                                                             ##
 ##                                                                            ##
@@ -23,53 +24,33 @@
 ##----------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------
 ## Script
-$SCRIPT_FULLPATH = $MyInvocation.MyCommand.Path;
-$SCRIPT_DIR      = Split-Path "$SCRIPT_FULLPATH" -Parent;
-$HOME_DIR        = if($HOME -eq "") { "$env:USERPROFILE" } else { $HOME };
-## Program
-$PROGRAM_NAME              = "repochecker";
-$DIRECTORY_NAME            = "${PROGRAM_NAME}_";
-$PROGRAM_SOURCE_PATH       = "$SCRIPT_DIR/$PROGRAM_NAME";
-$PROGRAM_INSTALL_ROOT_PATH = "$HOME_DIR/.stdmatt/bin";
-$PROGRAM_INSTALL_SUB_PATH  = "$PROGRAM_INSTALL_ROOT_PATH/$DIRECTORY_NAME";
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 
+## Program
+PROGRAM_NAME="repochecker";
+
+PROGRAM_INSTALL_DIR="${HOME}/.mateus-earth/bin";
+
+SRC_PATH="${SCRIPT_DIR}/${PROGRAM_NAME}/main.py"
+INSTALL_PATH="${PROGRAM_INSTALL_DIR}/${PROGRAM_NAME}"
 
 ##----------------------------------------------------------------------------##
 ## Script                                                                     ##
 ##----------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------
-echo "Installing ...";
+echo "---> Installing ...";
 
 ## Create the install directory...
-if (-not (Test-Path -LiteralPath $PROGRAM_INSTALL_SUB_PATH)) {
-    echo "Creating directory at: ";
-    echo "    $PROGRAM_INSTALL_SUB_PATH";
-    $null = (New-Item -Path $PROGRAM_INSTALL_SUB_PATH -ItemType Directory -Force);
-}
+if [ ! -d "${PROGRAM_INSTALL_DIR}" ]; then
+    echo "---> Creating directory at: ";
+    echo "     ${PROGRAM_INSTALL_DIR}";
+fi;
 
 ## Copy the file to the install dir...
-Copy-Item -Force $PROGRAM_SOURCE_PATH/main.py     `
-                 $PROGRAM_INSTALL_SUB_PATH/main.py
+cp -f "$SRC_PATH" "$INSTALL_PATH";
 
-## @notice(stdmatt): [Program Launcher]
-##   Creating a batch file just that calls the actual program cause on Windows
-##   we can't call the python intepreter directly from the command line script...
-##   On Unix we just follow the same approach for consistency.
-$launcher_string   = "";
-$launcher_filename = "$PROGRAM_NAME";
-if($IsWindows) {
-    $launcher_string    = "@echo off `npython3 $PROGRAM_INSTALL_SUB_PATH/main.py %1 %2 %3 %4 %5 %6 %7 %8 %9";
-    $launcher_filename += ".bat";
-} else {
-    $launcher_string = "#!/usr/bin/env sh`npython3 $PROGRAM_INSTALL_SUB_PATH/main.py $@";
-}
+echo "---> $PROGRAM_NAME was installed at: ($PROGRAM_INSTALL_DIR)";
+echo "     You might need add it to the PATH";
 
-echo $launcher_string `
-    | Out-File -Encoding ASCII -FilePath $PROGRAM_INSTALL_ROOT_PATH/$launcher_filename;
-
-echo "$PROGRAM_NAME was installed at:";
-echo "    $PROGRAM_INSTALL_ROOT_PATH";
-echo "You might need add it to the PATH";
-
-echo "Done... ;D";
+echo "---> Done... ;D";
 echo "";
