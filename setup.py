d4cb87402731dd713c570348f7bc672b3261f31b
mateusdigital <hello@mateus.digital>
2025-04-15 12:12:23 -0300

    New setup...

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-##~---------------------------------------------------------------------------##
-##                               *       +                                    ##
-##                         '                  |                               ##
-##                     ()    .-.,="``"=.    - o -                             ##
-##                           '=/_       \     |                               ##
-##                        *   |  '=._    |                                    ##
-##                             \     `=./`,        '                          ##
-##                          .   '=.__.=' `='      *                           ##
-##                 +                         +                                ##
-##                      O      *        '       .                             ##
-##                                                                            ##
-##  File      : setup.py                                                      ##
-##  Project   : repochecker                                                   ##
-##  Date      : 2024-03-22                                                    ##
-##  License   : See project's COPYING.TXT for full info.                      ##
-##  Author    : mateus.digital <hello@mateus.digital>                         ##
-##  Copyright : mateus.digital - 2024                                         ##
-##                                                                            ##
-##  Description :                                                             ##
-##                                                                            ##
-##---------------------------------------------------------------------------~##
-
 ## -----------------------------------------------------------------------------
 from setuptools import setup, find_packages
 
@@ -27,11 +5,25 @@ from setuptools import setup, find_packages
 setup(
     name="repochecker",
     version="3.1.0",
-    description="Checks and displays informations about git repos.",
+    author="mateus.digital",
+    author_email="hello@mateus.digital",
+    description="A tool to check the status of git repositories and submodules.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mateusdigital/repochecker",  
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
     entry_points={
         "console_scripts": [
-            "repochecker = repochecker.main:run",
+            "repochecker=repochecker.main:run",  
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
